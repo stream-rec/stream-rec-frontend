f858179aa9d3f399c2833b6b71d5ab28ce5a36eb
hua0512 <whua0512@gmail.com>
2024-02-29 14:11:35 +0100

    feat(streamers): static streamers page

--- app/dashboard/components/cards/streamer.tsx
+++ app/dashboard/components/cards/streamer.tsx
@@ -5,6 +5,8 @@ import {format} from "date-fns";
 import {cn} from "@/lib/utils";
 import {clsx} from "clsx";
 import {Badge} from "@/components/new-york/ui/badge";
+import {Button} from "@/components/new-york/ui/button";
+import {SettingsIcon} from "lucide-react";
 
 type StreamerCardProps = {
   streamer: string;
@@ -29,7 +31,7 @@ export default function StreamerCard({
 
   const getLastStreamInfo = () => {
     if (isLive) {
-      return "Currently streaming"
+      return "Streaming"
     }
 
     let lastStreamDate = new Date(lastStream || 0)
@@ -41,44 +43,45 @@ export default function StreamerCard({
 
     // if last stream was today, get the hours
     if (lastStreamDate.toDateString() === nowDate.toDateString()) {
-      return `Last stream was ${lastStreamDate.getHours()} hours ago`
+      return `${lastStreamDate.getHours()} hours ago`
     }
     const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
     // if last stream was yesterday, display yesterday
     if (lastStreamDate.toDateString() === yesterday.toDateString()) {
-      return `Last stream was yesterday`
+      return `yesterday`
     }
     // else display the date
-    return `last stream was ${format(lastStreamDate, "dd/MM/yyyy")}`
+    return `${format(lastStreamDate, "dd/MM/yyyy")}`
   }
 
   return (
       <>
-        <Card className={"mx-auto"}>
-          <CardHeader className={"space-y-2"}>
-            <div className={"space-y-4 md:flex md:flex-row md:space-x-2.5 md:space-y-0.5"}>
-              <Avatar>
-                <AvatarImage src={streamerAvatar} alt={streamer}></AvatarImage>
-                <AvatarFallback>{streamer}</AvatarFallback>
-              </Avatar>
+        <div className={"relative"}>
+          <Card className={"mx-auto"}>
+            <CardHeader className={"space-y-2"}>
+              <div className={"space-y-4 md:flex md:flex-row md:space-x-2.5 md:space-y-0.5"}>
+                <Avatar>
+                  <AvatarImage src={streamerAvatar} alt={streamer}></AvatarImage>
+                  <AvatarFallback>{streamer}</AvatarFallback>
+                </Avatar>
 
-              <div className={"flex flex-col mt-0.5 space-y-1 justify-center"}>
-                <div className={"flex flex-row items-center space-x-2.5 gap-x-1"}>
-                  <CardTitle className={clsx(cn("2xl:text-sm"), {"md:self-center": description})}>{streamer}</CardTitle>
+                <div className={"flex flex-col mt-0.5 space-y-1 justify-center"}>
+                  <div className={"flex flex-row items-center space-x-2.5 gap-x-1"}>
+                    <CardTitle className={clsx(cn("2xl:text-sm"), {"md:self-center": description})}>{streamer}</CardTitle>
+                  </div>
+                  {description && <p className="text-[0.75rem] md:text-sm  text-muted-foreground">{description}</p>}
                 </div>
-                {description && <p className="text-[0.75rem] md:text-sm  text-muted-foreground">{description}</p>}
               </div>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div className="flex space-x-4 text-sm text-muted-foreground">
-              <div className="flex items-center">
-                <Badge variant={"secondary"}>{platform}</Badge>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-row items-center gap-1.5 mr-2">
+                <Badge variant={"default"}>{platform}</Badge>
+                <Badge variant={"secondary"}>{getLastStreamInfo()}</Badge>
               </div>
-              <p className={"text-[0.7rem] md:text-sm text-muted-foreground"}>{getLastStreamInfo()} </p>
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
+          <Button variant={"ghost"} className={"absolute top-0 end-1 rounded-full p-2"}><SettingsIcon className={"w-4 h-4"}/></Button>
+        </div>
       </>
   )
 }
--- app/dashboard/streamers/page.tsx
+++ app/dashboard/streamers/page.tsx
@@ -1,3 +1,37 @@
+import React from "react";
+import {placeholderStreamers} from "@/app/lib/placeholder-data";
+import StreamerCard from "@/app/dashboard/components/cards/streamer";
+import {Button} from "@/components/new-york/ui/button";
+import {PlusCircledIcon} from "@radix-ui/react-icons";
+
 export default function Page() {
-  return <p>Streamers Page</p>;
+  return <div className="flex-1 flex-col space-y-8 p-8 md:flex">
+
+    <div className="flex items-center justify-between space-y-2">
+      <div>
+        <h2 className="text-2xl font-bold tracking-tight">Streamers</h2>
+        <p className="text-muted-foreground">
+          List of streamers and their status.
+        </p>
+      </div>
+      <Button>
+        <PlusCircledIcon className="mr-2 h-4 w-4" />
+        Add streamer
+      </Button>
+    </div>
+
+    <div className={"grid gap-3 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-7"}>
+      {
+        placeholderStreamers.map(streamer => {
+          return <div key={streamer.id} className={"col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1"}>
+            <StreamerCard key={streamer.url} streamer={streamer.name} streamerId={streamer.id} streamerAvatar={streamer.avatar}
+                          isLive={streamer.isLive}
+                          isActivated={streamer.isActivated} platform={streamer.platform} lastStream={streamer.lastStream}
+                          description={streamer.description}
+            />
+          </div>
+        })
+      }
+    </div>
+  </div>
 }
