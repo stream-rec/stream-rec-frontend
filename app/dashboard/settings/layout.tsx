546a9812430e5f9d2da593331f9e5ddd2c93b879
hua0512 <whua0512@gmail.com>
2024-03-06 14:52:26 +0100

    fix(settings): error padding

--- app/dashboard/settings/layout.tsx
+++ app/dashboard/settings/layout.tsx
@@ -25,7 +25,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({children}: SettingsLayoutProps) {
   return (
       <>
-        <div className="space-y-6 p-10 pb-16">
+        <div className="space-y-6 p-8 pb-16">
           <div className="space-y-0.5">
             <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
             <p className="text-muted-foreground">
