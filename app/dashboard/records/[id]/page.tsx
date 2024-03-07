7a2c9ad6341c576a129e7bbb8397c93c6043d09c
hua0512 <whua0512@gmail.com>
2024-03-07 12:07:40 +0100

    fix(records): remove static generation

--- app/dashboard/records/[id]/page.tsx
+++ app/dashboard/records/[id]/page.tsx
@@ -2,11 +2,6 @@ import {fetchStream, fetchStreams} from "@/app/lib/data/streams/stream-apis";
 import {RecordsDetails} from "@/app/dashboard/records/components/records-details";
 import {fetchUploadResults} from "@/app/lib/data/uploads/upload-apis";
 
-export async function generateStaticParams() {
-  const streams = await fetchStreams()
-  return streams.map(stream => ({params: {id: stream.id.toString()}}))
-}
-
 export default async function Page({params}: { params: { id: string } }) {
 
   const {id} = params
