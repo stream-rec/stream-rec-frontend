94132b1cbe6370b8c4d044f923463ec26a6d01d2
hua0512 <whua0512@gmail.com>
2024-03-13 14:37:04 +0100

    feat: enable redirect from root to dashboard in next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: '/',
-    //             destination: '/dashboard',
-    //             permanent: true,
-    //         },
-    //     ]
-    // },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: true,
+            },
+        ]
+    },
     output: 'standalone',
 };
 
