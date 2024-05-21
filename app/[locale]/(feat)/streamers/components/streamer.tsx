67ae61651d42c1ebdc1f1d30286e28e35a518b9e
hua0512 <whua0512@gmail.com>
2024-05-21 12:38:29 +0200

    refactor(streamer.tsx): remove unnecessary div wrapper, adjust layout and styles
    - Removed unnecessary surrounding div in the streamer component
    - Adjusted layout and styling details for improved usability and consistency
    - Optimized text size and line clamping for streamer and lastStream fields
    - Modified Marquee speed and added spacing for better text readability

--- app/[locale]/(feat)/streamers/components/streamer.tsx
+++ app/[locale]/(feat)/streamers/components/streamer.tsx
@@ -65,83 +65,82 @@ export function StreamerCard({
 
   return (
       <>
-        <div className={"relative"}>
-          <Card className={"mx-auto"}>
-            <div className={"flex flex-row items-center mr-2 justify-end space-x-0.5 md:space-x-1"}>
-              {duration && <> <p className={"text-muted-foreground text-[0.70rem]"}>{duration}</p></>}
-              {bitrate && <>
-                  <Separator orientation={"vertical"} className={"h-4"}/>
-                  <p className={"text-muted-foreground text-[0.70rem]"}>{bitrate} kbps</p>
-              </>}
-              <Link href={`/streamers/${streamerId}/edit`}>
-                <Button variant={"ghost"} size={"icon"} className={"rounded-full p-2"}><SettingsIcon
-                    className={"w-4 h-4"}/></Button>
-              </Link>
-              <Separator orientation={"vertical"} className={"h-4"}/>
-              <DeleteIconDialog
-                  icon={<Button variant={"ghost"} size={"icon"} className={"rounded-full p-2"}><Trash2Icon className={"w-4 h-4"}/></Button>}
-                  onDelete={
-                    async () => {
-                      toast.promise(deleteStreamer(streamerId.toString()), {
-                        loading: `Deleting ${streamer}...`,
-                        success: () => {
-                          router.refresh()
-                          return "Streamer deleted"
-                        },
-                        error: (error) => `An error occurred while deleting streamer : ${error}`
-                      })
-                    }
-                  }/>
-            </div>
-            <CardHeader className={"pt-0 pb-3 lg:pb-6"}>
-              <div className={"space-y-4 md:flex md:flex-row md:space-x-2.5 items-center md:space-y-0"}>
-                {
-                    downloadUrl && <OpenVideoContextMenu url={downloadUrl} string={contextMenuStrings}>
-                        <Avatar onClick={openUrl(liveUrl)} className={"cursor-pointer"}>
-                            <AvatarImage src={streamerAvatar ?? ""} alt={streamer}></AvatarImage>
-                            <AvatarFallback>{streamer}</AvatarFallback>
-                        </Avatar>
-                    </OpenVideoContextMenu>
-                } {!downloadUrl && <Avatar>
-                  <AvatarImage src={streamerAvatar ?? ""} alt={streamer}></AvatarImage>
-                  <AvatarFallback>{streamer}</AvatarFallback>
-              </Avatar>
-              }
-                <div className={"flex flex-col mt-0 space-y-1"}>
-                  <div className={"flex flex-row items-center space-x-2.5 gap-x-1"}>
-                    <CardTitle
-                        className={clsx(cn("2xl:text-sm"), {"md:self-center": description})}>{streamer}</CardTitle>
-                  </div>
-                  {description &&
-                      <div
-                          onMouseEnter={() => setIsHovered(true)}
-                          onMouseLeave={() => setIsHovered(false)}
-                      >
-                          <Marquee speed={40} play={isHovered}>
-                              <p className="text-[0.75rem] md:text-sm text-muted-foreground pr-5">{description}</p>
-                          </Marquee>
-                      </div>
+        <Card>
+          <div className={"flex flex-row items-center mr-2 justify-end space-x-0.5 md:space-x-1"}>
+            {duration && <> <p className={"text-muted-foreground text-[0.70rem]"}>{duration}</p></>}
+            {bitrate && <>
+                <Separator orientation={"vertical"} className={"h-4"}/>
+                <p className={"text-muted-foreground text-[0.70rem]"}>{bitrate} kbps</p>
+            </>}
+            <Link href={`/streamers/${streamerId}/edit`}>
+              <Button variant={"ghost"} size={"icon"} className={"rounded-full p-2"}><SettingsIcon
+                  className={"w-4 h-4"}/></Button>
+            </Link>
+            <Separator orientation={"vertical"} className={"h-4"}/>
+            <DeleteIconDialog
+                icon={<Button variant={"ghost"} size={"icon"} className={"rounded-full p-2"}><Trash2Icon className={"w-4 h-4"}/></Button>}
+                onDelete={
+                  async () => {
+                    toast.promise(deleteStreamer(streamerId.toString()), {
+                      loading: `Deleting ${streamer}...`,
+                      success: () => {
+                        router.refresh()
+                        return "Streamer deleted"
+                      },
+                      error: (error) => `An error occurred while deleting streamer : ${error}`
+                    })
                   }
+                }/>
+          </div>
+          <CardHeader className={"pt-0 pb-3 lg:pb-6"}>
+            <div className={"space-y-4 md:flex md:flex-row md:space-x-2.5 items-center md:space-y-0"}>
+              {
+                  downloadUrl && <OpenVideoContextMenu url={downloadUrl} string={contextMenuStrings}>
+                      <Avatar onClick={openUrl(liveUrl)} className={"cursor-pointer"}>
+                          <AvatarImage src={streamerAvatar ?? ""} alt={streamer}></AvatarImage>
+                          <AvatarFallback>{streamer}</AvatarFallback>
+                      </Avatar>
+                  </OpenVideoContextMenu>
+              } {!downloadUrl && <Avatar>
+                <AvatarImage src={streamerAvatar ?? ""} alt={streamer}></AvatarImage>
+                <AvatarFallback>{streamer}</AvatarFallback>
+            </Avatar>
+            }
+              <div className={"flex flex-col space-y-1"}>
+                <div className={"relative flex flex-row items-center space-x-2.5 gap-x-1"}>
+                  <CardTitle className={clsx(cn("2xl:text-sm line-clamp-1"), {"md:self-center": description})}>
+                    {streamer}
+                  </CardTitle>
                 </div>
-              </div>
-            </CardHeader>
-            <CardContent className={"pt-0"}>
-              <div className="flex flex-row items-center gap-x-1.5 mr-2">
-                {!template && (
-                    <>
-                      <Badge variant={"default"}>{platform}</Badge>
-                      <Badge variant={"secondary"}>{lastStream}</Badge>
-                    </>
-                )}
-                {
-                    template && (
-                        <Badge variant={"default"}>{template}</Badge>
-                    )
+                {description &&
+                    <div
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                    >
+                        <Marquee className={"text-[0.75rem] md:text-sm text-muted-foreground"} speed={55} play={isHovered}>
+                          {description.concat("   ")}
+                        </Marquee>
+                    </div>
                 }
               </div>
-            </CardContent>
-          </Card>
-        </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-row items-center gap-x-1.5 mr-2">
+              {!template && (
+                  <>
+                    <Badge variant={"default"}>{platform}</Badge>
+                    <Badge variant={"secondary"} className={"line-clamp-2"}>{lastStream}</Badge>
+                  </>
+              )}
+              {
+                  template && (
+                      <Badge variant={"default"}>{template}</Badge>
+                  )
+              }
+            </div>
+          </CardContent>
+        </Card>
       </>
   )
 }
