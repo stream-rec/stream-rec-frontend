c01ce37142a0ae131b841ba0e24c13d3a559ae77
hua0512 <whua0512@gmail.com>
2024-05-23 13:50:28 +0200

    feat(UI): add new screen breakpoint and adjust streamer list grid
    - Add new '3xl' screen size at '1600px' in `tailwind.config.ts`
    - Adjust grid columns for screen sizes in `streamer-list-wrapper.tsx` to accommodate the new '3xl' size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        "3xl": "1600px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
