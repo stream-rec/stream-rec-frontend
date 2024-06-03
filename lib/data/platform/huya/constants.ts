4860574ea669d96c81600f7857ebb742f2b97efb
hua0512 <whua0512@gmail.com>
2024-06-03 12:24:02 +0200

    chore(huya): update CDN line options for Huya in zh.json and constants.ts
    - Modified the "cdnDescription" entry in the zh.json file to update the CDN line options for Huya
    - Altered the huyaCDNs export constant in constants.ts to reflect these changes

--- lib/data/platform/huya/constants.ts
+++ lib/data/platform/huya/constants.ts
@@ -1,4 +1,4 @@
-export const huyaCDNs = ["AL", "TX", "HW", "WS", "HS", "AL13", "HW16", "HY"] as const;
+export const huyaCDNs = ["AL", "HS", "HW", "HY", "HYZJ", "TX", "WS"] as const;
 
 export const huyaProfileRoomRegex = new RegExp("profileRoom\"\\s*:\\s*(\\d+)")
 export const huyaRegex = "(?:https?://)?(?:(?:www|m)\\.)?huya\\.com/([a-zA-Z0-9]+)"
