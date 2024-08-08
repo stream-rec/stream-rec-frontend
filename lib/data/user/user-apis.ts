f06a1e6bc3c813cc7818b3c6668b77c9acfde996
hua <whua0512@gmail.com>
2024-08-08 09:56:26 +0200

    feat(user): hash passwords with md5 before sending to API (#42)
    - add `createHash` import from `crypto`
    - introduce `hashMd5` function to hash passwords
    - update `login` function to send hashed password

--- lib/data/user/user-apis.ts
+++ lib/data/user/user-apis.ts
@@ -1,14 +1,20 @@
 'use server'
 
 import {fetchApi} from "@/lib/data/api";
+import {createHash} from 'crypto';
+
+const hashMd5 = (input: string): string => {
+  return createHash('md5').update(input).digest('hex');
+};
 
 export const login = async (username: string, password: string) => {
+  const hashedPassword = hashMd5(password)
   const response = await fetchApi('/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({username, password})
+    body: JSON.stringify({username, password: hashedPassword})
   })
 
   if (!response.ok) {
