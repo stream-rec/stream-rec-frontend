4db2a384f728171ad0bdd2777dac52df2f9f04e7
hua0512 <whua0512@gmail.com>
2024-09-28 14:44:45 +0200

    feat(douyu-platform): support `hs-h5` cdn

--- lib/data/platform/douyu/constants.ts
+++ lib/data/platform/douyu/constants.ts
@@ -1,2 +1,2 @@
 export const douyuRegex = "^https:\\/\\/www\\.douyu\\.com.*"
-export const douyuCdns = ["ws-h5", "tctc-h5", "tct-h5", "ali-h5", "hw-h5", "akm-h5"] as const
+export const douyuCdns = ["ws-h5", "tctc-h5", "tct-h5", "ali-h5", "hw-h5", "hs-h5"] as const
