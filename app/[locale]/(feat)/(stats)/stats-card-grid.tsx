16a30c1ebc0c4cf980403cfcc7570d790566cb89
hua0512 <whua0512@gmail.com>
2024-10-07 21:39:20 +0200

    refactor(streamer-skeleton): update skeleton sizes

--- app/[locale]/(feat)/(stats)/stats-card-grid.tsx
+++ app/[locale]/(feat)/(stats)/stats-card-grid.tsx
@@ -3,8 +3,7 @@ import StatsSkeleton from "@/app/[locale]/(feat)/(stats)/stats-skeleton";
 import {TotalStatsCard, WeeklyStatsCard} from "@/app/[locale]/(feat)/(stats)/dashboard-cards";
 
 
-export default async function StatsCardGrid() {
-
+export default function StatsCardGrid() {
   return (
       <div className="grid gap-4 md:grids-col-2 lg:grid-cols-2 lg:gap-6 xl:grid-cols-2 xl:gap-x-16">
         <div className="space-y-4 lg:col-span-1">
--- app/[locale]/(feat)/streamers/components/record-list-skeleton.tsx
+++ app/[locale]/(feat)/streamers/components/record-list-skeleton.tsx
@@ -10,19 +10,22 @@ export type RecordListSkeletonProps = {
 export default function RecordListSkeleton({title}: RecordListSkeletonProps) {
 
   const generateSkeleton = (id: number) => {
-    return <Skeleton key={id}>
-      <div className="flex flex-row items-center space-x-4 p-10">
-        <Skeleton className="h-12 w-12 rounded-full"/>
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-12 md:w-40 lg:w-20 xl:w-12"/>
-          <Skeleton className="h-4 w-12 md:w-40 lg:w-20 xl:w-12"/>
+    return <Skeleton key={id} className={"rounded-xl"}>
+      <div className={"flex flex-col space-y-1.5 p-6"}>
+        <div className="flex flex-row items-center py-6 gap-x-5">
+          <Skeleton className="h-12 w-12 rounded-full"/>
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-12 md:w-40 lg:w-20 xl:w-20"/>
+            <Skeleton className="h-4 w-12 md:w-40 lg:w-20 xl:w-20"/>
+          </div>
         </div>
       </div>
+
     </Skeleton>
   }
   return (
       <>
-        <div className={"space-y-4"}>
+        <div className={"space-y-3"}>
           <div className="flex items-center space-x-4 justify-between">
             <h3 className="text-xm font-semibold"> {title}</h3>
             <h3 className="text-xs px-2 py-0.5 bg-muted rounded-md">0</h3>
@@ -38,7 +41,7 @@ export default function RecordListSkeleton({title}: RecordListSkeletonProps) {
 
           </div>
 
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 xl:gap-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-1 2xl:grid-cols-2 xl:gap-6">
             {
               Array.from({length: 5}, (_, i) => generateSkeleton(i))
             }
