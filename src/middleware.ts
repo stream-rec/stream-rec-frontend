cfe81992580a00c5d5394f0f842c4487dda24392
hua0512 <whua0512@gmail.com>
2025-01-17 14:50:00 +0100

    feat(middleware): enhance logging and improve request handling for public and protected pages
    - Added console logging to track request paths and authentication status.
    - Improved handling of public and protected page requests, ensuring proper redirection to login paths.
    - Enhanced clarity in middleware logic for better debugging and maintenance.

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,25 +22,31 @@ export default auth((req) => {
     'i'
   );
 
+  console.log("req.nextUrl.pathname:", req.nextUrl.pathname)
+
   const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
 
   // for public pages, pass through intlMiddleware
   if (isPublicPage) {
+    console.log("public page:", req.nextUrl.pathname)
     return intlMiddleware(req);
   }
   // for protected pages, redirect to login page if not logged
   else if (!req.auth && req.nextUrl.pathname !== "/login") {
-    // console.log("req:", req)
+    console.log("not logged in, protected page:", req.nextUrl.pathname)
     // if the user is already on the login page, pass through intlMiddleware
     if (req.nextUrl.pathname === BASE_PATH + '/login') { 
+      console.log("inside login page")
       return intlMiddleware(req);
     }
     // find locale from pathname
     const locale = routing.locales.find(l => req.nextUrl.pathname.startsWith(`/${l}`));
     const loginPath = locale ? `/${locale}/login` : '/login';
     const finalPath = BASE_PATH + loginPath
+    console.log("finalPath:", finalPath)
     return Response.redirect(new URL(finalPath, req.nextUrl.origin))
   }
+  console.log("logged in, protected page:", req.nextUrl.pathname)
   // pass through intlMiddleware for logged in users
   return intlMiddleware(req);
 })
