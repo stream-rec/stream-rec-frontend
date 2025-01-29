20817b2371d2c044b176a21f32a159958f504750
hua0512 <whua0512@gmail.com>
2025-01-29 11:09:48 +0100

    feat(streamers-card): add a hack for Huya stream URL processing

--- src/app/[locale]/(feat)/streamers/utils/streamer-utils.tsx
+++ src/app/[locale]/(feat)/streamers/utils/streamer-utils.tsx
@@ -1,15 +1,15 @@
-import {StreamerSchema, StreamerState} from "@/src/lib/data/streams/definitions";
-import {deleteStreamer, updateState} from "@/src/lib/data/streams/streamer-apis";
-import {StreamerCard, StreamerCardProps} from "@/src/app/[locale]/(feat)/streamers/components/streamer-card";
-import {OpenVideoContextMenuStrings} from "@/src/app/[locale]/(feat)/streamers/components/open-video-context-menu";
+import { StreamerSchema, StreamerState } from "@/src/lib/data/streams/definitions";
+import { deleteStreamer, updateState } from "@/src/lib/data/streams/streamer-apis";
+import { StreamerCard, StreamerCardProps } from "@/src/app/[locale]/(feat)/streamers/components/streamer-card";
+import { OpenVideoContextMenuStrings } from "@/src/app/[locale]/(feat)/streamers/components/open-video-context-menu";
 import React from "react";
-import {useFormatter, useTranslations} from "next-intl";
+import { useFormatter, useTranslations } from "next-intl";
 
 export const toStreamerCards = (streamers: StreamerSchema[], t: ReturnType<typeof useTranslations>, format: ReturnType<typeof useFormatter>) => {
   const formatLastStream = (streamer: StreamerSchema) => {
 
     if (streamer.state !== StreamerState.CANCELLED && streamer.state !== StreamerState.NOT_LIVE) {
-      return t("state", {state: streamer.state});
+      return t("state", { state: streamer.state });
     }
 
     const lastStream = streamer.lastLiveTime
@@ -33,14 +33,14 @@ export const toStreamerCards = (streamers: StreamerSchema[], t: ReturnType<typeo
       liveUrl: streamer.url,
       deleteStreamer: deleteStreamer
     }
-  }) as StreamerCardProps []
+  }) as StreamerCardProps[]
 }
 
 export const toStreamerCard = (
-    streamer: StreamerCardProps,
-    contextMenuStrings: OpenVideoContextMenuStrings,
-    updateStatusAction: (id: string, status: StreamerState) => ReturnType<typeof updateState>,
-    deleteStreamerAction: (id: string) => ReturnType<typeof deleteStreamer>
+  streamer: StreamerCardProps,
+  contextMenuStrings: OpenVideoContextMenuStrings,
+  updateStatusAction: (id: string, status: StreamerState) => ReturnType<typeof updateState>,
+  deleteStreamerAction: (id: string) => ReturnType<typeof deleteStreamer>
 ) => <StreamerCard
     key={streamer.streamerId}
     streamer={streamer.streamer}
@@ -55,21 +55,43 @@ export const toStreamerCard = (
     template={streamer.template}
     bitrate={streamer.bitrate}
     duration={streamer.duration}
-    downloadUrl={streamer.downloadUrl}
+    downloadUrl={processDownloadUrl(streamer.downloadUrl)}
     liveUrl={streamer.liveUrl}
     contextMenuStrings={contextMenuStrings}
-/>;
+  />;
 
 
 export function toResponsiveCard(
-    streamer: StreamerCardProps,
-    contextMenuStrings: OpenVideoContextMenuStrings,
-    updateStatusAction: (id: string, status: StreamerState) => ReturnType<typeof updateState>,
-    deleteStreamerAction: (id: string) => ReturnType<typeof deleteStreamer>
+  streamer: StreamerCardProps,
+  contextMenuStrings: OpenVideoContextMenuStrings,
+  updateStatusAction: (id: string, status: StreamerState) => ReturnType<typeof updateState>,
+  deleteStreamerAction: (id: string) => ReturnType<typeof deleteStreamer>
 ) {
   return (
-      <div key={streamer.streamerId} className={"col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1"}>
-        {toStreamerCard(streamer, contextMenuStrings, updateStatusAction, deleteStreamerAction)}
-      </div>
+    <div key={streamer.streamerId} className={"col-span-2 md:col-span-2 lg:col-span-2 xl:col-span-1"}>
+      {toStreamerCard(streamer, contextMenuStrings, updateStatusAction, deleteStreamerAction)}
+    </div>
   );
+}
+
+function processDownloadUrl(url: string | undefined) {
+  if (!url) return undefined;
+  // check if its huya
+  if (!url.includes("huya.com")) {
+    return url;
+  }
+  const [baseUrl, queryString] = url.split('?');
+  if (!queryString) return url;
+
+  // THIS IS A HUYA HACK
+  const params = new URLSearchParams(queryString);
+  params.delete("codec")
+  params.delete("ctype");
+  params.delete("fs");
+  params.set("ctype", "tars_mp");
+  params.set("fs", "bgct");
+  params.set("codec", "264");
+  const finalUrl = `${baseUrl}?${params.toString()}`;
+
+  return finalUrl;
 }
