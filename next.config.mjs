2504aa1b3159e751143b02722de77e8bb47dff69
hua0512 <whua0512@gmail.com>
2025-02-14 21:34:06 +0100

    chore(config): Update minimum server version to 10552

--- next.config.mjs
+++ next.config.mjs
@@ -33,7 +33,7 @@ const nextConfig = {
 	output: "standalone",
 	env: {
 		APP_VERSION: appVersion,
-		MIN_SERVER_VERSION: "10532",
+		MIN_SERVER_VERSION: "10552",
 		AUTH_URL: process.env.NEXT_PUBLIC_BASE_URL,
 		AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
 	},
