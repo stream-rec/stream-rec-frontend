d573f542100d158d879a15157999aec10f347c8c
hua0512 <whua0512@gmail.com>
2025-02-25 09:46:17 +0100

    chore: remove settings padding

--- src/app/[locale]/(feat)/settings/layout.tsx
+++ src/app/[locale]/(feat)/settings/layout.tsx
@@ -29,7 +29,7 @@ export default function SettingsLayout({ children }: { children: React.ReactNode
 	return (
 		<>
 			<ContentLayout title={t("globalSettings")}>
-				<div className='space-y-6 p-8 pb-16'>
+				<div className='space-y-6 p-0 pb-16'>
 					<div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
 						<aside className='lg:w-1/8'>
 							<SidebarNav items={sidebarNavItems} />
