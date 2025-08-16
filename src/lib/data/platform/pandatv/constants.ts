1c3cf35af85ac4ead8e12330993e28086f11e84d
FeilongH <feilong18.ai@gmail.com>
2025-08-16 21:16:26 +0800

    fix(pandatv): update live link regex pattern (#53)

--- src/lib/data/platform/pandatv/constants.ts
+++ src/lib/data/platform/pandatv/constants.ts
@@ -1,1 +1,1 @@
-export const pandatvRegex = "(?:https?://)?(?:www\\.)?pandalive\\.co\\.kr/live/play/([a-zA-Z0-9]+)"
+export const pandatvRegex = "(?:https?://)?(?:www\\.)?pandalive\\.co\\.kr/(?:live/)?play/([a-zA-Z0-9]+)"
